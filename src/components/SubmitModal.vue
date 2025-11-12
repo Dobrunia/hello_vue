4cd75fd59a00651841694f20090ecee374e81e99
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2025-11-12 20:45:28 +0300

    feat: useScrollLock

--- src/components/SubmitModal.vue
+++ src/components/SubmitModal.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { OnClickOutside } from '@vueuse/components';
+import { useScrollLock } from '@vueuse/core';
+import { watch } from 'vue';
 
 const props = defineProps({
   isOpen: Boolean,
@@ -12,33 +14,37 @@ function close() {
     emit('modalClose');
   }
 }
+
+const isLocked = useScrollLock(document.body);
+
+watch(
+  () => props.isOpen,
+  open => isLocked.value = open
+);
 </script>
 
 <template>
-  <Transition name="modal">
-    <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center">
-      <!-- Backdrop (затемнение) -->
-      <div class="absolute inset-0 bg-black/50 backdrop-blur-sm" />
+  <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center">
+    <!-- Backdrop (затемнение) -->
+    <div class="absolute inset-0 bg-black/50 backdrop-blur-sm" />
 
-      <!-- Модальное окно -->
-      <OnClickOutside
-        @trigger="close"
-        class="relative w-[300px] h-[300px] z-10 bg-gray-700"
-        base-border
-        p-4
-      >
-        <!-- Крестик в правом верхнем углу -->
-        <button
-          class="absolute top-2 right-2 p-1"
-          base-hover
-          i-line-md:menu-to-close-transition
-          @click="close"
-        />
-        <slot />
-      </OnClickOutside>
-    </div>
-  </Transition>
+    <!-- Модальное окно -->
+    <OnClickOutside
+      @trigger="close"
+      class="relative w-[300px] h-[300px] z-10 bg-gray-700"
+      base-border
+      p-4
+    >
+      <!-- Крестик в правом верхнем углу -->
+      <button
+        class="absolute top-2 right-2 p-1"
+        base-hover
+        i-line-md:menu-to-close-transition
+        @click="close"
+      />
+      <slot />
+    </OnClickOutside>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
