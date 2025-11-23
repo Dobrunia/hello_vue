69d26394dba6fcbbb9a8d14361ce37faf3fcccc0
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2025-11-23 05:22:22 +0300

    feat: update PromptTextarea layout and add microphone icon with conditional rendering

--- src/components/PromptTextarea.vue
+++ src/components/PromptTextarea.vue
@@ -6,25 +6,41 @@ const prompt = defineModel<string>({ default: '' });
   <div flex="~ items-center justify-center" class="w-full h-63px">
     <div
       flex="~ items-center justify-between"
-      class="h-full w-864px bg-[#303030] border-1 border-[#3a3a3a] rounded-28px font-size-24px line-height-[1.5] pr-10px pl-20px shadow-short"
+      class="h-full w-864px bg-[#303030] border-1 border-[#3a3a3a] rounded-28px font-size-24px line-height-[1.5] pr-10px pl-16px shadow-short"
     >
       <div i-line-md:plus />
       <input
         class="w-80% h-50% translate-y-[-2px] whitespace-pre-wrap outline-none user-select-none"
         placeholder="Спросите что-нибудь…"
       />
-      <div i-tabler:microphone></div>
-      <div class="bg-[#424242] rounded-full p-2">
+      <div v-if="prompt.length < 1" flex="~ items-center justify-center gap-0.5rem">
+        <div i-tabler:microphone></div>
+        <div class="bg-[#424242] rounded-full p-2">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon"
+          >
+            <path
+              d="M7.167 15.416V4.583a.75.75 0 0 1 1.5 0v10.833a.75.75 0 0 1-1.5 0Zm4.166-2.5V7.083a.75.75 0 0 1 1.5 0v5.833a.75.75 0 0 1-1.5 0ZM3 11.25V8.75a.75.75 0 0 1 1.5 0v2.5a.75.75 0 0 1-1.5 0Zm12.5 0V8.75a.75.75 0 0 1 1.5 0v2.5a.75.75 0 0 1-1.5 0Z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+      <div v-if="prompt.length > 0">
         <svg
-          width="24"
-          height="24"
+          width="20"
+          height="20"
           viewBox="0 0 20 20"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
           class="icon"
         >
           <path
-            d="M7.167 15.416V4.583a.75.75 0 0 1 1.5 0v10.833a.75.75 0 0 1-1.5 0Zm4.166-2.5V7.083a.75.75 0 0 1 1.5 0v5.833a.75.75 0 0 1-1.5 0ZM3 11.25V8.75a.75.75 0 0 1 1.5 0v2.5a.75.75 0 0 1-1.5 0Zm12.5 0V8.75a.75.75 0 0 1 1.5 0v2.5a.75.75 0 0 1-1.5 0Z"
+            d="M8.99992 16V6.41407L5.70696 9.70704C5.31643 10.0976 4.68342 10.0976 4.29289 9.70704C3.90237 9.31652 3.90237 8.6835 4.29289 8.29298L9.29289 3.29298L9.36907 3.22462C9.76184 2.90427 10.3408 2.92686 10.707 3.29298L15.707 8.29298L15.7753 8.36915C16.0957 8.76192 16.0731 9.34092 15.707 9.70704C15.3408 10.0732 14.7618 10.0958 14.3691 9.7754L14.2929 9.70704L10.9999 6.41407V16C10.9999 16.5523 10.5522 17 9.99992 17C9.44764 17 8.99992 16.5523 8.99992 16Z"
           ></path>
         </svg>
       </div>
